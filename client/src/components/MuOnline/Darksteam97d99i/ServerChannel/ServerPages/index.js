aed9b39522cf10803d28de188ef82acdeca8adbe
Richtervn <tuanvuflp@gmail.com>
2017-10-09 10:03:44 +0700

    Quest Editor on work

--- client/src/components/MuOnline/Darksteam97d99i/ServerChannel/ServerPages/index.js
+++ client/src/components/MuOnline/Darksteam97d99i/ServerChannel/ServerPages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import BagItemsEditor from './BagItemsEditor';
 import ShopEditor from './ShopEditor';
+import QuestEditor from './QuestEditor';
 
 export default ({ page, data, onGetData }) => {
   switch (page) {
@@ -8,6 +9,8 @@ export default ({ page, data, onGetData }) => {
       return <BagItemsEditor data={data} onGetData={onGetData}/>;
     case 'Shop Editor':
       return <ShopEditor data={data} onGetData={onGetData}/>
+    case 'Quest Editor':
+      return <QuestEditor data={data} onGetData={onGetData}/>
     default:
       return null;
   }
