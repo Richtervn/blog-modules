63a7945814d82d9facfb9eb8c2da8d3a129e32e6
unknown <tuanvuflp@gmail.com>
2017-11-13 08:58:40 +0700

    [WIP] Progress socket

--- server/src/socket/MuOnline/Darksteam97d99i/WebQuestWorkers/index.js
+++ server/src/socket/MuOnline/Darksteam97d99i/WebQuestWorkers/index.js
@@ -3,8 +3,44 @@ import Promise from 'bluebird';
 import questList from './questList';
 
 import WQ1 from './WQ1';
+import WQ2 from './WQ2';
+import WQ3 from './WQ3';
+import WQ4 from './WQ4';
+import WQ5 from './WQ5';
+import WQ6 from './WQ6';
+import WQ7 from './WQ7';
+import WQ8 from './WQ8';
+import WQ9 from './WQ9';
+import WQ10 from './WQ10';
+import WQ11 from './WQ11';
+import WQ12 from './WQ12';
+import WQ13 from './WQ13';
+import WQ14 from './WQ14';
+import WQ15 from './WQ15';
+import WQ16 from './WQ16';
+import WQ17 from './WQ17';
+import WQ18 from './WQ18';
 
-const questWorkers = { WQ1 };
+const questWorkers = {
+	WQ1,
+	WQ2,
+	WQ3,
+	WQ4,
+	WQ5,
+	WQ6,
+	WQ7,
+	WQ8,
+	WQ9,
+	WQ10,
+	WQ11,
+	WQ12,
+	WQ13,
+	WQ14,
+	WQ15,
+	WQ16,
+	WQ17,
+	WQ18
+};
 
 class WebQuestWorker {
 	constructor(client, models, methods) {
@@ -34,39 +70,76 @@ class WebQuestWorker {
 		const userQuestIds = _.pluck(userWebQuest, 'quest_id');
 
 		await Promise.map(questList, async webQuest => {
-			if (!_.contains(userQuestIds, webQuest._id)) {
-				const questWorker = new questWorkers[webQuest._id]();
-				await questWorker.initial(
-					this.models,
-					this.methods,
-					this.membInfo,
-					this.characters,
-					this.banking,
-					this.membCredits,
-					webQuest
-				);
-				this.questWorkers[webQuest._id] = questWorker;
+			if (webQuest.type == 'Account' && !_.contains(userQuestIds, webQuest._id)) {
+				UserWebQuest.create({
+					memb___id: memb___id,
+					quest_id: webQuest._id,
+					finish_times: 0,
+					progress: 0,
+					status: 'start',
+					type: webQuest.isRepeatable ? 'repeatable' : 'onetime'
+				});
 			}
+
+			if(webQuest.type == 'Character')
+			// if(webQuest.type == 'Character'){
+
+			// 	UserWebQuest.create({
+			// 		memb___id: memb___id,
+			// 		quest_id: webQuest._id,
+			// 		finish_times: 0,
+			// 		progress: 0,
+			// 		status: 'start',
+			// 		type: webQuest.isRepeatable ? 'repeatable' : 'onetime'
+			// 	})
+			// }
+			// const questWorker = new questWorkers[webQuest._id]();
+			// await questWorker.initial(
+			// 	this.models,
+			// 	this.methods,
+			// 	this.membInfo,
+			// 	this.characters,
+			// 	this.banking,
+			// 	this.membCredits,
+			// 	webQuest
+			// );
+			// this.questWorkers[webQuest._id] = questWorker;
 		});
 	}
 }
 
 export default WebQuestWorker;
-// export WQ1 from './WQ1';
-// export WQ2 from './WQ2';
-// export WQ3 from './WQ3';
-// export WQ4 from './WQ4';
-// export WQ5 from './WQ5';
-// export WQ6 from './WQ6';
-// export WQ7 from './WQ7';
-// export WQ8 from './WQ8';
-// export WQ9 from './WQ9';
-// export WQ10 from './WQ10';
-// export WQ11 from './WQ11';
-// export WQ12 from './WQ12';
-// export WQ13 from './WQ13';
-// export WQ14 from './WQ14';
-// export WQ15 from './WQ15';
-// export WQ16 from './WQ16';
-// export WQ17 from './WQ17';
-// export WQ18 from './WQ18';
+
+// z    id: {
+//         type: DataTypes.INTEGER,
+//         autoIncrement: true,
+//         primaryKey: true
+//       },
+//       memb___id: {
+//         type: DataTypes.STRING,
+//         allowNull: true
+//       },
+//       character_name: {
+//         type: DataTypes.STRING,
+//         allowNull: true
+//       },
+//       quest_id: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false
+//       },
+//       finish_times: {
+//         type: DataTypes.INTEGER,
+//         allowNull: true
+//       },
+//       status: {
+//         type: DataTypes.STRING,
+//         allowNull: true
+//       },
+//       progress: {
+//         type: DataTypes.INTEGER,
+//         allowNull: true
+//       },
+//       type: {
+//         type: DataTypes.STRING,
+//         allowNull: true
+//       }
