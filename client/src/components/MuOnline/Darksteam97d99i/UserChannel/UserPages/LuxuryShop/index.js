b278ad5f2faa3377c9c55e6567c8f278f2e03277
unknown <tuanvuflp@gmail.com>
2017-11-27 00:47:25 +0700

    [WIP] Luxury Shop buy services

--- client/src/components/MuOnline/Darksteam97d99i/UserChannel/UserPages/LuxuryShop/index.js
+++ client/src/components/MuOnline/Darksteam97d99i/UserChannel/UserPages/LuxuryShop/index.js
@@ -17,8 +17,12 @@ export default ({
 }) => (
 	<div className="ds9799-page-wrapper">
 		<NavigatorBar pages={pages} currentPage={currentPage} onChangePage={onChangePage} />
-		{currentPage == 'Exchange' && <Exchange exchanges={exchanges} onGetExchange={onGetExchange} />}
-		{currentPage == 'Receipts' && <Receipt receipts={receipts} onGetReceipt={onGetReceipt} />}
-		{currentPage == 'Consumable' && <Consumable consumables={consumables} onGetConsumable={onGetConsumable} />}
+		<div className="row no-row-margin">
+			<div className="ds9799-lx-list-container">
+				{currentPage == 'Exchange' && <Exchange exchanges={exchanges} onGetExchange={onGetExchange} />}
+				{currentPage == 'Receipts' && <Receipt receipts={receipts} onGetReceipt={onGetReceipt} />}
+				{currentPage == 'Consumable' && <Consumable consumables={consumables} onGetConsumable={onGetConsumable} />}
+			</div>
+		</div>
 	</div>
 );
