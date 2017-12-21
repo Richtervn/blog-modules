1e232b280471cd1c0e970d4ca73e7c3741cfc316
unknown <tuanvuflp@gmail.com>
2017-12-22 03:36:11 +0700

    mirror working

--- client/src/components/Tools/ContentMirror/index.js
+++ client/src/components/Tools/ContentMirror/index.js
@@ -1,7 +1,7 @@
 import 'codemirror/theme/base16-dark.css';
 import 'codemirror/mode/jsx/jsx';
 
-import React from 'react';
+import React, { Component } from 'react';
 import CodeMirror from 'react-codemirror';
 import ContentSelector from './ContentSelector';
 
@@ -12,30 +12,45 @@ const options = {
   theme: 'base16-dark'
 };
 
-export default ({
-  selectedTable,
-  onChangeTable,
-  records,
-  onChangeRecord,
-  selectedRecord,
-  codeValue,
-  onChangeCodeValue,
-  onSaveCode,
-  onRefreshCode
-}) => (
-  <div className="home-main-screen">
-    <ContentSelector
-      selectedTable={selectedTable}
-      selectedRecord={selectedRecord}
-      records={records}
-      onChangeTable={onChangeTable}
-      onChangeRecord={onChangeRecord}
-    />
-    <div className="row no-row-margin">
-      <div className="col no-col-margin">
-        <CodeMirror options={options} value={codeValue} />
+class ContentMirror extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: ''
+    };
+    this.handleChangeCode = this.handleChangeCode.bind(this);
+  }
+
+  handleChangeCode(code){
+    this.setState({content: code});
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ content: nextProps.content });
+  }
+
+  render() {
+    const { selectedTable, selectedRecord, records, onChangeTable, onSaveCode, onChangeRecord } = this.props;
+    return (
+      <div className="home-main-screen">
+        <ContentSelector
+          selectedTable={selectedTable}
+          selectedRecord={selectedRecord}
+          records={records}
+          onChangeTable={onChangeTable}
+          onChangeRecord={onChangeRecord}
+        />
+        <div className="row no-row-margin">
+          <div className="col no-col-margin">
+            <CodeMirror options={options} value={this.state.content} onChange={this.handleChangeCode}/>
+          </div>
+          <div className="col no-col-margin">
+            <div dangerousSetInnerHtml={{__html: this.state.content}}/>
+          </div>
+        </div>
       </div>
-      <div className="col no-col-margin">.</div>
-    </div>
-  </div>
-);
+    );
+  }
+}
+
+export default ContentMirror;
--- client/src/modules/Tools/contentMirror.js
+++ client/src/modules/Tools/contentMirror.js
@@ -1,6 +1,24 @@
-export default (state = {}, action) => {
+import { tools } from 'services';
+import { toast } from 'react-toastify';
+
+export default (state = {
+  content: '',
+  selectedTable: 'default',
+  selectedRecord: 'default',
+  records: []
+}, action) => {
   switch(action.type){
     default:
       return state;
   }
 }
+
+  // selectedTable,
+  // onChangeTable,
+  // records,
+  // onChangeRecord,
+  // selectedRecord,
+  // codeValue,
+  // onChangeCodeValue,
+  // onSaveCode,
+  // onRefreshCode
