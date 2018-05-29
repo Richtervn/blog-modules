8b5cfd8e09d0ebc7e00432218d58dd3ad0d41ef7
unknown <tuanvuflp@gmail.com>
2018-05-30 03:21:15 +0700

    added ReceiptCard

--- client/src/pages/MuOnline/Darksteam97d99i/User/LuxuryShop/Receipt/Receipt.component.js
+++ client/src/pages/MuOnline/Darksteam97d99i/User/LuxuryShop/Receipt/Receipt.component.js
@@ -3,6 +3,8 @@ import _ from 'underscore';
 import React, { Component } from 'react';
 import { ContainerLoader } from 'common/Loaders';
 
+import { getItemImage, formatNumber } from 'helpers';
+
 class Receipt extends Component {
   componentWillMount() {
     const { receipts, onGetReceipts } = this.props;
@@ -18,6 +20,36 @@ class Receipt extends Component {
         <div className="receipts-list">
           {!receipts && <ContainerLoader />}
           {receipts && _.isEmpty(receipts) && <div className="empty-shop">Nothing to sell yet!</div>}
+          {receipts &&
+            receipts.map(receipt => (
+              <div key={receipt.id} className="wrapper">
+                <div className="receipt-card">
+                  <div className="label">{receipt.name}</div>
+                  <div className="image">
+                    <img src={receipt.image_url} alt={receipt.name} />
+                  </div>
+                  <div className="description">{receipt.description}</div>
+                  <div className="materials">
+                    <div className="label">Materials</div>
+                    {receipt.materials.map(material => (
+                      <div className="material" key={material.id}>
+                        {material.count} x&nbsp;
+                        <img
+                          src={getItemImage(material.category, material.itemId, material.level)}
+                          alt={material.name}
+                        />&nbsp;
+                        {material.name}
+                      </div>
+                    ))}
+                  </div>
+                  <div className="feature">
+                    <button className="btn btn-danger btn-block">
+                      Buy : <span>{formatNumber(receipt.price)}</span> Credits
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
         </div>
       </div>
     );
