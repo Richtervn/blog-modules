296e3ddb5eb3c44c08594b8f9637d5ce1c32bd88
unknown <tuanvuflp@gmail.com>
2018-06-14 01:44:00 +0700

    cleaner code

--- server/src/routes/mangasReading/services/crawl.js
+++ server/src/routes/mangasReading/services/crawl.js
@@ -24,42 +24,64 @@ const downloadImg = (url, filePath) =>
 const crawlTruyenTranhNet = url =>
   new Promise((resolve, reject) => {
     let form = {};
+
     const urlFrags = url
       .replace('http://', '')
       .replace('https://', '')
       .split('/');
 
+    const requestUrl = url
+      .split('/')
+      .filter((frag, i) => i <= 4)
+      .join('/');
+
     crawler.direct({
-      uri: url,
+      uri: requestUrl,
       callback: async (err, res) => {
         if (err) {
           return reject(err);
         }
         const $ = res.$;
         try {
-          form.Name = $('.title-manga')
-            .text()
-            .split(' ')
-            .map(text => toTitleCase(text))
-            .join(' ');
+          let name = $('.title-manga').text();
+          if (name) {
+            form.Name = name
+              .split(' ')
+              .map(text => toTitleCase(text))
+              .join(' ');
+          } else {
+            return reject({ message: 'Failed to crawl manga name' });
+          }
+
           form.Aka = [urlFragToAka(urlFrags[1])];
 
           const imgSrc = $('.media-left > img').attr('src');
+          if (!imgSrc) {
+            return reject({ message: 'Failed to crawl cover image' });
+          }
           const filename = `${moment().format('MMDDYYYYhhmmss')}.jpg`;
           const filepath = `./public/Mangas Reading/${filename}`;
           await downloadImg(imgSrc, filepath);
           form.CoverUri = filepath;
-          form.Chapter = '1'
+          form.Chapter = '1';
 
-          const detail = $('.description-update')
-            .text()
+          let detail = $('.description-update').text();
+          if (!detail) {
+            return reject({ message: 'Failed to crawl manga detail' });
+          }
+
+          detail = detail
             .replace(/\r/g, '')
             .trim()
             .split('\n')
             .map(txt => txt.trim());
+
           detail.forEach((text, i) => {
             if (text.indexOf('Tên khác') !== -1) {
-              const akas = detail[i + 1].replace(/;/g, ',').split(',').concat(form.Aka);
+              const akas = detail[i + 1]
+                .replace(/;/g, ',')
+                .split(',')
+                .concat(form.Aka);
               form.Aka = _.uniq(akas).join(',');
             }
             if (text.indexOf('Thể loại') !== -1) {
@@ -74,6 +96,7 @@ const crawlTruyenTranhNet = url =>
           form.Introduce = $('.manga-content p:nth-of-type(2)').text();
           form.ReadingUrl = url;
           form.Rating = 3;
+
           return resolve(form);
         } catch (e) {
           return reject(e);
