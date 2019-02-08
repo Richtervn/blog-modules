0a9c7aebabac78e223a0af710f6570f71a14ec7d
Richtervn <tuanvuflp@gmail.com>
2019-02-08 15:30:04 +0700

    use hook Page & HeaderMenu

--- client/src/common/Layout/HeaderMenu/HeaderMenu.component.js
+++ client/src/common/Layout/HeaderMenu/HeaderMenu.component.js
@@ -1,146 +1,119 @@
 import './HeaderMenu.css';
-import React, { Component } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import MiniMusicPlayer from './components/MiniMusicPlayer.container';
 
-class HeaderMenu extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: {
-        mangaUrl: '',
-        crawlMangaUrl: '',
-        newChapterUrl: '',
-        appDiary: ''
+export default ({ isShow, onQuickUpdateManga, onCrawlManga, onSaveNewMangaChapter, onAddAppDiary, onSelfHide }) => {
+  let wrapper = useRef(null);
+
+  const [mangaUrl, setMangaUrl] = useState('');
+  const [crawlMangaUrl, setCrawlMangaUrl] = useState('');
+  const [newChapterUrl, setNewChapterUrl] = useState('');
+  const [appDiary, setAppDiary] = useState('');
+
+  useEffect(() => {
+    const handleSelfHide = event => {
+      if (wrapper && !wrapper.contains(event.target)) {
+        event.stopImmediatePropagation();
+        onSelfHide();
       }
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSelfHide = this.handleSelfHide.bind(this);
-  }
 
-  handleChange(event) {
-    const { name, value } = event.target;
-    const valueState = { ...this.state.value };
-    switch (name) {
-      default:
-        valueState[name] = value;
-        break;
-    }
-    this.setState({ value: valueState });
-  }
-
-  handleQuickUpdateManga() {
-    this.props.onQuickUpdateManga({ url: this.state.value.mangaUrl });
-    this.setState({ ...this.state, value: { ...this.state.value, mangaUrl: '' } });
-  }
-
-  handleAddAppDiary() {
-    this.props.onAddAppDiary({ text: this.state.value.appDiary });
-    this.setState({ ...this.state, value: { ...this.state.value, appDiary: '' } });
-  }
-
-  handleSaveNewMangaChapter() {
-    this.props.onSaveNewMangaChapter({ url: this.state.value.newChapterUrl });
-    this.setState({ ...this.state, value: { ...this.state.value, newChapterUrl: '' } });
-  }
-
-  handleCrawlManga() {
-    this.props.onCrawlManga({ url: this.state.value.crawlMangaUrl });
-    this.setState({ ...this.state, value: { ...this.state.value, crawlMangaUrl: '' } });
-  }
-
-  handleSelfHide(event) {
-    if (this.wrapper && !this.wrapper.contains(event.target)) {
-      event.stopImmediatePropagation();
-      this.props.onSelfHide();
-    }
-  }
-
-  render() {
-    const { isShow } = this.props;
     if (isShow) {
-      document.addEventListener('click', this.handleSelfHide);
+      document.addEventListener('click', handleSelfHide);
     }
-    if (this.wrapper && !isShow) {
-      document.removeEventListener('click', this.handleSelfHide);
+    if (wrapper && !isShow) {
+      document.removeEventListener('click', handleSelfHide);
     }
+  }, [isShow]);
 
-    if (!isShow) return null;
-
-    return (
-      <div className="header-menu-bar" ref={node => (this.wrapper = node)}>
-        <div className="header-menu-input-wrapper">
-          <input
-            type="text"
-            className="form-control header-menu-input"
-            name="mangaUrl"
-            value={this.state.value.mangaUrl}
-            onChange={this.handleChange}
-            placeholder="Quick update manga reading url"
-          />
-          <button
-            className="btn btn-success header-menu-btn"
-            disabled={!this.state.value.mangaUrl}
-            onClick={() => this.handleQuickUpdateManga()}>
-            <i className="fa fa-plus-circle" />
-          </button>
-        </div>
-
-        <div className="header-menu-input-wrapper-next">
-          <input
-            type="text"
-            className="form-control header-menu-input"
-            name="crawlMangaUrl"
-            value={this.state.value.crawlMangaUrl}
-            onChange={this.handleChange}
-            placeholder="Crawl manga reading url"
-          />
-          <button
-            className="btn btn-info header-menu-btn"
-            disabled={!this.state.value.crawlMangaUrl}
-            onClick={() => this.handleCrawlManga()}>
-            <i className="fa fa-plus-circle" />
-          </button>
-        </div>
-
-        <div className="header-menu-input-wrapper-next">
-          <input
-            type="text"
-            className="form-control header-menu-input"
-            name="newChapterUrl"
-            value={this.state.value.newChapterUrl}
-            onChange={this.handleChange}
-            placeholder="Quick update manga reading url"
-          />
-          <button
-            className="btn btn-warning header-menu-btn"
-            disabled={!this.state.value.newChapterUrl}
-            onClick={() => this.handleSaveNewMangaChapter()}>
-            <i className="fa fa-plus-circle" />
-          </button>
-        </div>
-
-        <div className="header-menu-input-wrapper-next">
-          <input
-            type="text"
-            className="form-control header-menu-input"
-            name="appDiary"
-            value={this.state.value.appDiary}
-            onChange={this.handleChange}
-            placeholder="Add App Diary"
-          />
-          <button
-            className="btn btn-primary header-menu-btn"
-            disabled={!this.state.value.appDiary}
-            onClick={() => this.handleAddAppDiary()}>
-            <i className="fa fa-send-o" />
-          </button>
-        </div>
-        <div className="header-menu-mini-player">
-          <MiniMusicPlayer />
-        </div>
-      </div>
-    );
+  if (!isShow) {
+    return null;
   }
-}
 
-export default HeaderMenu;
+  return (
+    <div className="header-menu-bar" ref={node => (wrapper = node)}>
+      <div className="header-menu-input-wrapper">
+        <input
+          type="text"
+          className="form-control header-menu-input"
+          name="mangaUrl"
+          value={mangaUrl}
+          onChange={e => setMangaUrl(e.target.value)}
+          placeholder="Quick update manga reading url"
+        />
+        <button
+          className="btn btn-success header-menu-btn"
+          disabled={!mangaUrl}
+          onClick={() => {
+            onQuickUpdateManga({ url: mangaUrl });
+            setMangaUrl('');
+          }}>
+          <i className="fa fa-plus-circle" />
+        </button>
+      </div>
+
+      <div className="header-menu-input-wrapper-next">
+        <input
+          type="text"
+          className="form-control header-menu-input"
+          name="crawlMangaUrl"
+          value={crawlMangaUrl}
+          onChange={e => setCrawlMangaUrl(e.target.value)}
+          placeholder="Crawl manga reading url"
+        />
+        <button
+          className="btn btn-info header-menu-btn"
+          disabled={!setCrawlMangaUrl}
+          onClick={() => {
+            onCrawlManga({ url: crawlMangaUrl });
+            setCrawlMangaUrl('');
+          }}>
+          <i className="fa fa-plus-circle" />
+        </button>
+      </div>
+
+      <div className="header-menu-input-wrapper-next">
+        <input
+          type="text"
+          className="form-control header-menu-input"
+          name="newChapterUrl"
+          value={newChapterUrl}
+          onChange={e => setNewChapterUrl(e.target.value)}
+          placeholder="Quick update manga reading url"
+        />
+        <button
+          className="btn btn-warning header-menu-btn"
+          disabled={!newChapterUrl}
+          onClick={() => {
+            onSaveNewMangaChapter({ url: newChapterUrl });
+            setNewChapterUrl('');
+          }}>
+          <i className="fa fa-plus-circle" />
+        </button>
+      </div>
+
+      <div className="header-menu-input-wrapper-next">
+        <input
+          type="text"
+          className="form-control header-menu-input"
+          name="appDiary"
+          value={appDiary}
+          onChange={e => setAppDiary(e.target.value)}
+          placeholder="Add App Diary"
+        />
+        <button
+          className="btn btn-primary header-menu-btn"
+          disabled={!appDiary}
+          onClick={() => {
+            onAddAppDiary({ text: appDiary });
+            setAppDiary('');
+          }}>
+          <i className="fa fa-send-o" />
+        </button>
+      </div>
+      <div className="header-menu-mini-player">
+        <MiniMusicPlayer />
+      </div>
+    </div>
+  );
+};
--- client/src/common/Layout/Page/Page.component.js
+++ client/src/common/Layout/Page/Page.component.js
@@ -1,47 +1,33 @@
 import './Page.css';
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { appRouter } from 'utils';
 
-class Page extends Component {
-  constructor(props) {
-    super(props);
-    let isRecievedTree;
-    if (this.props.menuTree) {
-      isRecievedTree = false;
-    } else {
-      isRecievedTree = true;
+export default ({
+  children,
+  menuTree,
+  activeItem,
+  activeGroup,
+  onSetActiveGroup,
+  onSetActiveItem,
+  onSetDefaultShowGroup,
+  onToggleMenuGroup
+}) => {
+  useEffect(() => {
+    if (!menuTree) {
+      return;
     }
-    this.state = { isRecievedTree };
-  }
+    const router = appRouter(menuTree);
+    const currentPage = router.decode(window.location.pathname);
 
-  componentWillReceiveProps(nextProps) {
-    const { onSetActiveGroup, onSetActiveItem, onSetDefaultShowGroup, onToggleMenuGroup } = this.props;
-    const { menuTree, activeItem, activeGroup } = nextProps;
-
-    if (menuTree && !this.state.isRecievedTree) {
-      this.setState({ isRecievedTree: true });
+    if (activeItem !== currentPage.activeItem) {
+      onSetActiveItem(currentPage.activeItem);
     }
-
-    if (menuTree && this.state.isRecievedTree) {
-      const router = appRouter(menuTree);
-      const currentPage = router.decode(window.location.pathname);
-
-      if (activeItem !== currentPage.activeItem) {
-        onSetActiveItem(currentPage.activeItem);
-      }
-      if (activeGroup !== currentPage.activeGroup) {
-        onSetActiveGroup(currentPage.activeGroup);
-        onSetDefaultShowGroup(currentPage.activeGroup);
-        onToggleMenuGroup(currentPage.activeGroup);
-      }
+    if (activeGroup !== currentPage.activeGroup) {
+      onSetActiveGroup(currentPage.activeGroup);
+      onSetDefaultShowGroup(currentPage.activeGroup);
+      onToggleMenuGroup(currentPage.activeGroup);
     }
-  }
+  }, [menuTree]);
 
-  render() {
-    const { children } = this.props;
-
-    return <div className="page">{children}</div>;
-  }
-}
-
-export default Page;
+  return <div className="page">{children}</div>;
+};
