ed754c85a610489bfb14c05b7f8895eccedca961
Richtervn <tuanvuflp@gmail.com>
2019-02-08 18:39:08 +0700

    added header menu validators

--- client/src/common/Layout/HeaderMenu/HeaderMenu.component.js
+++ client/src/common/Layout/HeaderMenu/HeaderMenu.component.js
@@ -1,6 +1,58 @@
 import './HeaderMenu.css';
 import React, { useState, useRef, useEffect } from 'react';
+import classnames from 'classnames';
+import { validators } from 'helpers/form';
+
 import MiniMusicPlayer from './components/MiniMusicPlayer.container';
+
+const HeaderRowForm = ({
+  onSubmit,
+  value,
+  onChange,
+  validator,
+  placeholder,
+  buttonClass,
+  iconClass = 'fa-plus-circle',
+  disabled
+}) => {
+  const [error, setError] = useState(false);
+  const [changed, setChanged] = useState(false);
+
+  return (
+    <form
+      className="header-menu-input-wrapper"
+      onSubmit={e => {
+        e.preventDefault();
+        if (!error) {
+          onSubmit();
+        }
+      }}>
+      <input
+        type="text"
+        className={classnames('form-control header-menu-input', { 'is-valid': changed && !error, 'is-invalid': error })}
+        value={value}
+        onChange={e => {
+          if (!changed) {
+            setChanged(true);
+          } else {
+            if (!validator) {
+              return;
+            }
+            const isValid = validator(e.target.value);
+            if (!isValid !== error) {
+              setError(!isValid);
+            }
+          }
+          onChange(e.target.value);
+        }}
+        placeholder={placeholder}
+      />
+      <button className={classnames('btn', buttonClass, 'header-menu-btn')} disabled={disabled} type="submit">
+        <i className={classnames('fa', iconClass)} />
+      </button>
+    </form>
+  );
+};
 
 export default ({ isShow, onQuickUpdateManga, onCrawlManga, onSaveNewMangaChapter, onAddAppDiary, onSelfHide }) => {
   let wrapper = useRef(null);
@@ -17,7 +69,6 @@ export default ({ isShow, onQuickUpdateManga, onCrawlManga, onSaveNewMangaChapte
         onSelfHide();
       }
     };
-
     if (isShow) {
       document.addEventListener('click', handleSelfHide);
     }
@@ -32,85 +83,54 @@ export default ({ isShow, onQuickUpdateManga, onCrawlManga, onSaveNewMangaChapte
 
   return (
     <div className="header-menu-bar" ref={node => (wrapper = node)}>
-      <div className="header-menu-input-wrapper">
-        <input
-          type="text"
-          className="form-control header-menu-input"
-          name="mangaUrl"
-          value={mangaUrl}
-          onChange={e => setMangaUrl(e.target.value)}
-          placeholder="Quick update manga reading url"
-        />
-        <button
-          className="btn btn-success header-menu-btn"
-          disabled={!mangaUrl}
-          onClick={() => {
-            onQuickUpdateManga({ url: mangaUrl });
-            setMangaUrl('');
-          }}>
-          <i className="fa fa-plus-circle" />
-        </button>
-      </div>
-
-      <div className="header-menu-input-wrapper-next">
-        <input
-          type="text"
-          className="form-control header-menu-input"
-          name="crawlMangaUrl"
-          value={crawlMangaUrl}
-          onChange={e => setCrawlMangaUrl(e.target.value)}
-          placeholder="Crawl manga reading url"
-        />
-        <button
-          className="btn btn-info header-menu-btn"
-          disabled={!setCrawlMangaUrl}
-          onClick={() => {
-            onCrawlManga({ url: crawlMangaUrl });
-            setCrawlMangaUrl('');
-          }}>
-          <i className="fa fa-plus-circle" />
-        </button>
-      </div>
-
-      <div className="header-menu-input-wrapper-next">
-        <input
-          type="text"
-          className="form-control header-menu-input"
-          name="newChapterUrl"
-          value={newChapterUrl}
-          onChange={e => setNewChapterUrl(e.target.value)}
-          placeholder="Quick update manga reading url"
-        />
-        <button
-          className="btn btn-warning header-menu-btn"
-          disabled={!newChapterUrl}
-          onClick={() => {
-            onSaveNewMangaChapter({ url: newChapterUrl });
-            setNewChapterUrl('');
-          }}>
-          <i className="fa fa-plus-circle" />
-        </button>
-      </div>
-
-      <div className="header-menu-input-wrapper-next">
-        <input
-          type="text"
-          className="form-control header-menu-input"
-          name="appDiary"
-          value={appDiary}
-          onChange={e => setAppDiary(e.target.value)}
-          placeholder="Add App Diary"
-        />
-        <button
-          className="btn btn-primary header-menu-btn"
-          disabled={!appDiary}
-          onClick={() => {
-            onAddAppDiary({ text: appDiary });
-            setAppDiary('');
-          }}>
-          <i className="fa fa-send-o" />
-        </button>
-      </div>
+      <HeaderRowForm
+        value={mangaUrl}
+        disabled={!mangaUrl}
+        placeholder="Quick update manga reading url"
+        buttonClass="btn-success"
+        onChange={v => setMangaUrl(v)}
+        validator={v => validators.url(v)}
+        onSubmit={() => {
+          onQuickUpdateManga({ url: mangaUrl });
+          setMangaUrl('');
+        }}
+      />
+      <HeaderRowForm
+        value={crawlMangaUrl}
+        disabled={!crawlMangaUrl}
+        placeholder="Crawl manga url"
+        buttonClass="btn-info"
+        onChange={v => setCrawlMangaUrl(v)}
+        onSubmit={() => {
+          onCrawlManga({ url: crawlMangaUrl });
+          setCrawlMangaUrl('');
+        }}
+      />
+      <HeaderRowForm
+        value={newChapterUrl}
+        disabled={!newChapterUrl}
+        placeholder="New manga's chapter url"
+        buttonClass="btn-warning"
+        onChange={v => setNewChapterUrl(v)}
+        validator={v => validators.url(v)}
+        onSubmit={() => {
+          onSaveNewMangaChapter({ url: newChapterUrl });
+          setNewChapterUrl('');
+        }}
+      />
+      <HeaderRowForm
+        value={appDiary}
+        disabled={!appDiary}
+        placeholder="Add app diary"
+        buttonClass="btn-primary "
+        iconClass="fa-send-o"
+        onChange={v => setAppDiary(v)}
+        validator={v => v && v !== ''}
+        onSubmit={() => {
+          onAddAppDiary({ text: appDiary });
+          setAppDiary('');
+        }}
+      />
       <div className="header-menu-mini-player">
         <MiniMusicPlayer />
       </div>
--- client/src/helpers/form/index.js
+++ client/src/helpers/form/index.js
@@ -2,5 +2,6 @@ import commonAddArray from './commonAddArray';
 import commonRemoveArray from './commonRemoveArray';
 import commonFormChange from './commonFormChange';
 import commonValidate from './commonValidate';
+import validators from './validators';
 
-export { commonAddArray, commonRemoveArray, commonFormChange, commonValidate };
+export { commonAddArray, commonRemoveArray, commonFormChange, commonValidate, validators };
